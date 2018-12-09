26ebda19c86b6de24f121a9a9516ea45987a5ce5
Brandon <hall.brandon@hotmail.com>
2018-12-08 20:22:17 -0500

    updated changes

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 
 var spotify = new Spotify(keys.spotify);
+
+var request = require('request');
+
+var fs = require('fs');
+
+var axios = require("axios");
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
